afe90ef5c57446c1db7da4b869e5b6f0fd3fe5e7
sadatkhan-codes <md.sadatkhan2023@gmail.com>
2026-08-31 14:10:42 +0530

    Prime nos

--- Lecture codes/check_prime_nos.cpp
+++ Lecture codes/check_prime_nos.cpp
@@ -13,7 +13,7 @@ int main() {
 
     bool isPrime = true;
 
-    for (int i = 2; i <= n - 1; i++) {
+    for (int i = 2; i*i <= n; i++) {
         if (n % i == 0) {
             isPrime = false;
             break;
