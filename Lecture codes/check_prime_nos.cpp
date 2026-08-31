d89c842c965c02568eb244b505d3b8db3921f301
sadatkhan-codes <md.sadatkhan2023@gmail.com>
2026-08-31 13:59:20 +0530

    Prime nos

---
+++ Lecture codes/check_prime_nos.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 1) {
+        cout << "The number is neither prime nor composite\n";
+        return 0;
+    }
+
+    bool isPrime = true;
+
+    for (int i = 2; i <= n - 1; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        cout << "The number is prime\n";
+    } else {
+        cout << "The number is composite\n";
+    }
+
+    return 0;
+}
